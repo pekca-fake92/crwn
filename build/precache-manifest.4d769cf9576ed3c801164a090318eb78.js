4c9162dc36ec7d2721b0c59e2ea9fed4125a949c
pekca-fake92 <pekca-fake92@users.noreply.github.com>
2020-07-27 15:30:41 +0530

    fresh template with firebase anf gh-pages

---
+++ build/precache-manifest.4d769cf9576ed3c801164a090318eb78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b299eaf86efdd7a125eb45b350b148c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f949647acd31646d8b12",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49c425f0aeae79772bf9",
+    "url": "/static/js/2.b3376396.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b3376396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f949647acd31646d8b12",
+    "url": "/static/js/main.971be8b3.chunk.js"
+  },
+  {
+    "revision": "c65b2fc82bc88365a277",
+    "url": "/static/js/runtime-main.fc07d308.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
