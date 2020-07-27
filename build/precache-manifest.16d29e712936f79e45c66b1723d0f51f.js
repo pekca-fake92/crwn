fb90266aaf8a2e4e7b54d784affe7ff9174458f7
pekca-fake92 <pekca-fake92@users.noreply.github.com>
2020-07-27 17:31:03 +0530

    commit

--- build/precache-manifest.16d29e712936f79e45c66b1723d0f51f.js
+++ build/precache-manifest.16d29e712936f79e45c66b1723d0f51f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d411520932f771d47bb84791780e893",
+    "revision": "7f1e0527275033eef0c1db5953759e7d",
     "url": "/index.html"
   },
   {
-    "revision": "cc182656c5fd79c99814",
-    "url": "/static/css/main.27fd15f6.chunk.css"
+    "revision": "963c1f031119c2707a16",
+    "url": "/static/css/main.b68f1fd3.chunk.css"
   },
   {
     "revision": "4e27e7ff4a8b421f8bc5",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.203e3fb8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cc182656c5fd79c99814",
-    "url": "/static/js/main.9692ecec.chunk.js"
+    "revision": "963c1f031119c2707a16",
+    "url": "/static/js/main.f7804b6b.chunk.js"
   },
   {
     "revision": "c65b2fc82bc88365a277",
