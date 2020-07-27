77c87153e399d0d5dab2bd32823b182494be346c
pekca-fake92 <pekca-fake92@users.noreply.github.com>
2020-07-27 20:22:28 +0530

    Updates

---
+++ precache-manifest.debd0583530ac09bc1ef239ababcd143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c9a22d304538f35c7223212f284e90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9cdf7b26c4e7507038c",
+    "url": "/static/css/main.8eaaa694.chunk.css"
+  },
+  {
+    "revision": "11fd3a3db3f805a451f4",
+    "url": "/static/js/2.0e5b798e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0e5b798e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9cdf7b26c4e7507038c",
+    "url": "/static/js/main.b976ab3f.chunk.js"
+  },
+  {
+    "revision": "c65b2fc82bc88365a277",
+    "url": "/static/js/runtime-main.fc07d308.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.16d29e712936f79e45c66b1723d0f51f.js"
+  "/precache-manifest.debd0583530ac09bc1ef239ababcd143.js"
 );
 
 self.addEventListener('message', (event) => {
