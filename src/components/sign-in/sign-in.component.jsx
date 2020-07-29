70b376de0111fe5fd46262a2736693d025d259ab
pekca-fake92 <pekca-fake92@users.noreply.github.com>
2020-07-29 16:43:25 +0530

    signup

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -17,10 +17,15 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async(event) => {
     event.prevetDefault();
-
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
@@ -51,11 +56,11 @@ class SignIn extends React.Component {
             label="password"
             handleChange={this.handleChange}
             required
-          />
+          /> 
 
           <div className="buttons">
             <CustomButton type="submit">Sign-In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
               {" "}
               Google Sign-In{" "}
             </CustomButton>
